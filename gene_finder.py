d07980d6bd54302419fb17bf060d344076c0ecc9
Duncan Hall <duncan.hall@students.olin.edu>
2016-02-07 22:39:48 -0500

    submitting my genefinder build and modified amino_acids.py

--- gene_finder.py
+++ gene_finder.py
@@ -7,11 +7,15 @@ YOUR HEADER COMMENT HERE
 """
 
 import random
-# from amino_acids import aa, codons, aa_table    # you may find these useful
-# from load import load_seq
+from amino_acids import aa_table  # , aa, valid_codons
+from datetime import datetime
+from load import load_seq
+
+dna = load_seq('./data/X73525.fa')
+print('started compile  %s' % datetime.now())
 
 
-def shuffle_string(s):
+def get_shuffled_string(s):
     """Shuffles the characters in the input string
         NOTE: this is a helper function, you do not
         have to modify this in any way """
@@ -33,10 +37,15 @@ start_codon = 'ATG'
 
 
 def get_codons(dna):
-    codons = []
-    for i in range(0, len(dna), 3):
-        codons.append(dna[i:i + 3])
-    return codons
+    ''' Returns a list of codons corresponding to the dna string passed in
+    >>> get_codons('ATGCATGAATGTAG')
+    ['ATG', 'CAT', 'GAA', 'TGT']
+    '''
+    codons = [dna[i:i+3] for i in range(0, len(dna), 3)]
+    if len(codons[-1]) != 3:
+        return codons[:-1]
+    else:
+        return codons[:]
 
 
 def get_complement(nucleotide):
@@ -50,12 +59,8 @@ def get_complement(nucleotide):
     'A'
     >>> get_complement('C')
     'G'
-    >>> get_complement('G')     #
+    >>> get_complement('G')
     'C'
-    >>> get_complement('*')     # function catches anything which is not ATCG
-    '-'
-
-    # - makes the error easily visible in a string
     """
     # DONE: completes doctest
 
@@ -69,11 +74,11 @@ def get_complement(nucleotide):
     #     return 'G'
     # else:
     #     return '-'
-
-    if nucleotide in nucleotide_complements:
+    try:
         return nucleotide_complements[nucleotide]
-    else:
-        return '-'
+    except:
+        raise Exception('DNA input contains invalid character')
+        # this isn't working...
 
 
 def get_reverse_complement(dna):
@@ -86,19 +91,19 @@ def get_reverse_complement(dna):
     'AAAGCGGGCAT'
     >>> get_reverse_complement("CCGCGTTCA")
     'TGAACGCGG'
-    >>> get_reverse_complement("CCGCGDTTCA")    # check with error in input dna
-    'TGAA-CGCGG'
     """
     # DONE: completes doctest
-    complement = ""
 
-    for i in xrange(len(dna)):
-        complement_base = get_complement(dna[i])
-        complement += complement_base
-    return complement[::-1]
+    return ''.join([get_complement(nucleotide) for nucleotide in dna[::-1]])
+
+    # complement = ""
+    # for i in xrange(len(dna)):
+    #     complement_base = get_complement(dna[i])
+    #     complement += complement_base
+    # return complement[::-1]
 
 
-def rest_of_ORF(dna):
+def get_rest_of_ORF(dna):
     """ Takes a DNA sequence that is assumed to begin with a start_codon
         codon and returns the sequence up to but not including the
         first in frame stop codon.  If there is no in frame stop codon,
@@ -109,13 +114,13 @@ def rest_of_ORF(dna):
 
         added tests: for TAG which is out of place, and multiple stop codons
 
-    >>> rest_of_ORF("ATGTGAA")
+    >>> get_rest_of_ORF("ATGTGAA")
     'ATG'
-    >>> rest_of_ORF("ATGAGATAGG")
+    >>> get_rest_of_ORF("ATGAGATAGG")
     'ATGAGA'
-    >>> rest_of_ORF("ATGAGATTAGG")
+    >>> get_rest_of_ORF("ATGAGATTAGG")
     'ATGAGATTAGG'
-    >>> rest_of_ORF("ATGAGATAGTGA")
+    >>> get_rest_of_ORF("ATGAGATAGTGA")
     'ATGAGA'
     """
     # DONE: completes doctest
@@ -128,7 +133,7 @@ def rest_of_ORF(dna):
     return dna
 
 
-def find_all_ORFs_oneframe(dna):
+def get_all_ORFs_oneframe(dna):
     """ Finds all non-nested open reading frames in the given DNA
         sequence and returns them as a list.  This function should
         only find ORFs that are in the default frame of the sequence
@@ -141,32 +146,43 @@ def find_all_ORFs_oneframe(dna):
 
         added test: dna with nested start codon, and with non-default frame
 
-    >>> find_all_ORFs_oneframe("ATGCATGAATGTAGATAGATGTGCCC")
+    >>> get_all_ORFs_oneframe("ATGCATGAATGTAGATAGATGTGCCC")
     ['ATGCATGAATGTAGA', 'ATGTGCCC']
-    >>> find_all_ORFs_oneframe("ATGCATATGGAATGTAGATAGATGTGCCC")
-    ['ATGCATATGGAATGTAGA', 'ATGTGCCC']
-    >>> find_all_ORFs_oneframe("TATGCATATGGAATGGGGTAGATAGATGTGCCC")
+    >>> get_all_ORFs_oneframe("ATGCATATGGAATGTAGATAGATGTGCCC")
+    ['ATGCATATGGAATGTAGA', 'ATGGAATGTAGA', 'ATGTGCCC']
+    >>> get_all_ORFs_oneframe("TATGCATATGGAATGGGGTAGATAGATGTGCCCA")
     ['ATGGGG']
     """
     # DONE: completes doctest
-    orfs_list = []
-    codon_index = 0
     dna_codons = get_codons(dna)
 
-    while codon_index < len(dna_codons):
-        if dna_codons[codon_index] == start_codon:
-            an_orf = rest_of_ORF(dna[codon_index * 3:])
-            orfs_list.append(an_orf)
-            codon_index += len(an_orf) / 3
-        else:
-            codon_index += 1
+    return [get_rest_of_ORF(dna[index*3:])
+            for index in range(len(dna_codons))
+            if dna_codons[index] == start_codon]
 
-    return orfs_list
+    # orfs_list = []
+    # for index in range(len(dna_codons)):
+    #     if dna_codons[index] == start_codon:
+    #         an_orf = get_rest_of_ORF(dna[index*3:])
+    #         orfs_list.append(an_orf)
+    #         index += len(an_orf) / 3  # also skips the end codon
+    # return orfs_list
+
+    # index = 0
+    # while index/3 < len(dna_codons):
+    #     if dna_codons[index / 3] == start_codon:
+    #         an_orf = get_rest_of_ORF(dna[index:])
+    #         orfs_list.append(an_orf)
+    #         index += len(an_orf)
+    #     else:
+    #         index += 3
+
+    # return orfs_list
 
     # while dna:
     #     dna_codons = get_codons(dna)
     #     if dna_codons[index] == start_codon:
-    #         an_orf = rest_of_ORF(dna)
+    #         an_orf = get_rest_of_ORF(dna)
     #         orfs_list.append(an_orf)
     #         dna = dna[len(an_orf)::]
     #     else:
@@ -175,7 +191,7 @@ def find_all_ORFs_oneframe(dna):
     # return orfs_list
 
 
-def find_all_ORFs(dna):
+def get_all_ORFs(dna):
     """ Finds all non-nested open reading frames in the given DNA sequence in
         all 3 possible frames and returns them as a list.  By non-nested we
         mean that if an ORF occurs entirely within another ORF and they are
@@ -185,32 +201,34 @@ def find_all_ORFs(dna):
         dna: a DNA sequence
         returns: a list of non-nested ORFs
 
-    >>> find_all_ORFs("ATGCATGAATGTAG")
+    >>> get_all_ORFs("ATGCATGAATGTAG")
     ['ATGCATGAATGTAG', 'ATGAATGTAG', 'ATG']
-    >>> find_all_ORFs("-ATGCATGAATGTAG")
+    >>> get_all_ORFs("-ATGCATGAATGTAG")
     ['ATGCATGAATGTAG', 'ATGAATGTAG', 'ATG']
-    >>> find_all_ORFs("--ATGCATGAATGTAG")
+    >>> get_all_ORFs("--ATGCATGAATGTAG")
     ['ATGCATGAATGTAG', 'ATGAATGTAG', 'ATG']
     """
     # DONE: completes doctest
     # all_orfs = []
 
-    for offset in xrange(3):
-        return sum([find_all_ORFs_oneframe(dna[offset:]) for offset in [0, 1, 2]], [])
+    return sum([get_all_ORFs_oneframe(dna[offset:])
+                for offset
+                in [0, 1, 2]],
+               [])
 
     # for offset in xrange(3):
-    #     some_orfs = find_all_ORFs_oneframe(offset * "-" + dna)
+    #     some_orfs = get_all_ORFs_oneframe(offset * "-" + dna)
     #     all_orfs.extend(some_orfs)
     # all_orfs.sort(key=len, reverse=True)  # for test
     # return all_orfs
 
 
-def find_all_reverse_ORFs(dna):
-    reverse_dna = get_reverse_complement(dna)
-    return find_all_ORFs(reverse_dna)
+# def get_all_reverse_ORFs(dna):
+#     reverse_dna = get_reverse_complement(dna)
+#     return get_all_ORFs(reverse_dna)
 
 
-def find_all_ORFs_both_strands(dna):
+def get_all_ORFs_both_strands(dna):
     """ Finds all non-nested open reading frames in the given DNA sequence on both
         strands.
 
@@ -219,65 +237,93 @@ def find_all_ORFs_both_strands(dna):
 
         no more tests added because of extensive testing at lower levels, and
         because this is a basic function
-    >>> find_all_ORFs_both_strands("ATGCGAATGTAGCATCAAA")
+    >>> get_all_ORFs_both_strands("ATGCGAATGTAGCATCAAA")
     ['ATGCGAATG', 'ATGCTACATTCGCAT']
     """
     # DONE: completes doctest
-    return sum([find_all_ORFs(strand) for strand in [dna, get_reverse_complement(dna)]], [])
+    return sum([get_all_ORFs(strand)
+               for strand
+               in [dna, get_reverse_complement(dna)]], [])
     # much mo' better
-    # return find_all_ORFs(dna) + find_all_reverse_ORFs(dna)
+    # return get_all_ORFs(dna) + get_all_reverse_ORFs(dna)
 
 
-def longest_ORF(dna):
+def get_longest_ORF(dna):
     """ Finds the longest ORF on both strands of the specified DNA and returns it
         as a string
-    >>> longest_ORF("ATGCGAATGTAGCATCAAA")
+    >>> get_longest_ORF("ATGCGAATGTAGCATCAAA")
     'ATGCTACATTCGCAT'
     """
-    # TODO: implement this
-    pass
+    # DONE: completes doctest
+    # if shuffle_instance != 0 and shuffle_instance % 10 == 0:
+    #     print('finding longest ORF in shuffle %s' % shuffle_instance)
+    try:
+        return max(get_all_ORFs_both_strands(dna), key=len)
+    except:
+        pass
 
 
-def longest_ORF_noncoding(dna, num_trials):
+def get_longest_ORF_noncoding(dna, num_trials):
     """ Computes the maximum length of the longest ORF over num_trials shuffles
         of the specfied DNA sequence
 
         dna: a DNA sequence
         num_trials: the number of random shuffles
-        returns: the maximum length longest ORF """
-    # TODO: implement this
-    pass
+        returns: the maximum length longest ORF
+        >>> get_longest_ORF_noncoding('TGTAGAC', 1000)
+        'no correct answer'
+        """
+    # Done: should be safe as well
+    shuffled_dna = [get_shuffled_string(dna) for _ in range(num_trials)]
+    try:
+        long_ORFS = [get_longest_ORF(dna) for dna in shuffled_dna
+                     if get_longest_ORF(dna) is not None]
+    except:
+        raise RuntimeError('shuffle produced no ORFs. Solution: re-run')
+    # only records shuffles which result in ORFs
+    return max(long_ORFS, key=len)
 
 
-def coding_strand_to_AA(dna):
+def get_AA_from_coding_strand(dna):
     """ Computes the Protein encoded by a sequence of DNA.  This function
         does not check for start and stop codons (it assumes that the input
-        DNA sequence represents an protein coding region).
+        DNA sequence represents a protein coding region).
 
         dna: a DNA sequence represented as a string
         returns: a string containing the sequence of amino acids encoded by the
                  the input DNA fragment
 
-        >>> coding_strand_to_AA("ATGCGA")
+        >>> get_AA_from_coding_strand("ATGCGA")
         'MR'
-        >>> coding_strand_to_AA("ATGCCCGCTTT")
+        >>> get_AA_from_coding_strand("ATGCCCGCTTT")
         'MPA'
     """
-    # TODO: implement this
-    pass
+    # Done: passes doctests
+
+    return ''.join([aa_table[codon] for codon in get_codons(dna)])
+    # I have verified that all possible codons are included in aa_table
 
 
-def gene_finder(dna):
+def get_probable_AA_sequence(dna):
     """ Returns the amino acid sequences that are likely coded by the specified dna
 
         dna: a DNA sequence
         returns: a list of all amino acid sequences coded by the sequence dna.
+    >>> get_probable_AA_sequence("ATGCCCGCTTT")
     """
-    # TODO: implement this
-    pass
+    threshold_length = len(get_longest_ORF_noncoding(dna, 1500))
+    longest_ORF = get_longest_ORF(dna)
+    if len(longest_ORF) >= threshold_length:
+        return get_AA_from_coding_strand(longest_ORF)
+    else:
+        return 'No one gene found with confidence.'
 
 if __name__ == "__main__":
-    import doctest
-    # doctest.testmod()
-    doctest.run_docstring_examples(
-        find_all_ORFs_both_strands, globals(), verbose=True)
+    start_time = datetime.now()
+    print(get_probable_AA_sequence(dna))
+    print('finished execution in %s' % (datetime.now() - start_time))
+    # import doctest
+    # # doctest.testmod()
+    # doctest.run_docstring_examples(get_reverse_complement,
+    #                                globals(), verbose=True,
+    #                                name="Jus' Testin'")
